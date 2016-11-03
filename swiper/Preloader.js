878be0aad97e44d857c137b27cea644431308585
Erikmannen <erik.azp@hotmail.se>
2016-11-03 13:52:51 +0100

    meny

--- swiper/Preloader.js
+++ swiper/Preloader.js
@@ -11,7 +11,7 @@ Preloader.prototype = {
         this.load.setPreloadSprite(this.preloadBar);
         this.titleText = this.add.image(this.world.centerX, this.world.centerY-220, 'titleimage');
         this.titleText.anchor.setTo(0.5, 0.5);
-        this.load.image('titlescreen', 'assets/images/TitleBG.png');
+        this.load.image('titlescreen', 'assets/images/ässomebgb2.png');
         this.load.bitmapFont('eightbitwonder', 'assets/fonts/eightbitwonder.png', 'assets/fonts/eightbitwonder.fnt');
         this.load.image('hill', 'assets/images/hill.png');
         this.load.image('sky', 'assets/images/sky.png');
@@ -23,11 +23,18 @@ Preloader.prototype = {
         this.load.audio('hurt_audio', 'assets/audio/hurt.mp3');
         this.load.audio('select_audio', 'assets/audio/select.mp3');
         this.load.audio('game_audio', 'assets/audio/bgm.mp3');
+        this.load.spritesheet('button', 'assets/images/spritesheets/Start_knapp.png');
     
 },
 	create: function () {
-       this.game.state.start('StartMenu');
+        this.game.state.start('StartMenu');
+        titlescreen = this.add.sprite(32, 32, 'titlescreen');
         //this.preloadBar.cropEnabled = false;
+        titlescreen.x = 0;
+        titlescreen.y = 0;
+        titlescreen.smoothed = false;
+        titlescreen.height = 350;
+        titlescreen.width = 250;
     },
  
     
