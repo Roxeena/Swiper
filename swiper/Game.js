938d1ea6f5f629fe329b435ae41e9c473b864057
Malin <malin.ejdbo@gmail.com>
2016-11-03 13:59:26 +0100

    städat

--- swiper/Game.js
+++ swiper/Game.js
@@ -4,11 +4,10 @@ Game = function(game) {
     this.secondsElapsed;
     this.timer;
     this.generateImage;
-    };
+};
 
 var bunny;
 var spacefighter;
-
 Game.prototype = {
 
    
@@ -17,36 +16,32 @@ Game.prototype = {
         this.secondsElapsed = 0;
         this.timer = this.time.create(false);
         this.timer.loop(1000, this.updateSeconds, this);
-       // this.game.time.events.loop(Phaser.Timer.SECOND, updateSeconds, this);
+        //this.game.time.events.loop(Phaser.Timer.SECOND, updateSeconds, this);
 
         //fysik statar t.ex. fysik
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity.y = 100;
 
-
+        //bygg upp världen
         this.buildWorld();
-
-        
     },
     
     updateSeconds: function() {
         this.secondsElapsed++;       
-    
-        },
+    },
 
     
     
     buildWorld: function() {
-       
+
         //addar bakgrund hill och sky
         this.add.image(0, 0, 'sky');
         this.add.image(0, 800, 'hill');
+
         //musiken skapas och spelas
         var music;
         music = this.game.add.audio('jerry');
         music.play();
-
-
 
         //startar timer som events beror på
         this.timer.start();
@@ -56,9 +51,6 @@ Game.prototype = {
 
         //quit
         this.game.time.events.add(Phaser.Timer.SECOND *68, this.quitGame, this);
-
-        
-
     },
 
     generateImage: function() {
@@ -68,15 +60,13 @@ Game.prototype = {
         rndnr=rndnr +this.game.rnd.integerInRange(1, 4);
         
 
-        if ( rndnr==1){
-
+        if (rndnr==1){
             arrowRight = this.game.add.sprite(this.game.world.randomX,0,'bunny');  
-            this.game.physics.enable( [ arrowRight], Phaser.Physics.ARCADE);
+            this.game.physics.enable( [ arrowRight ], Phaser.Physics.ARCADE);
             arrowRight.body.collideWorldBounds = true; 
             arrowRight.inputEnabled = true;
             arrowRight.input.enableDrag(true);
             arrowRight.input.allowVerticalDrag = false;
-
         }
         else if(rndnr==2){
             arrowLeft = this.game.add.sprite(this.game.world.randomX,0,'spacefighter');
@@ -85,38 +75,33 @@ Game.prototype = {
             arrowLeft.inputEnabled = true;
             arrowLeft.input.enableDrag(true);
             arrowLeft.input.allowVerticalDrag = false;
-           
         }
         else if(rndnr==3){
             arrowUp = this.game.add.sprite(this.game.world.randomX,0,'explosion');
-            this.game.physics.enable( [ arrowUp], Phaser.Physics.ARCADE);
+            this.game.physics.enable( [ arrowUp ], Phaser.Physics.ARCADE);
             arrowUp.body.collideWorldBounds = true;
             arrowUp.inputEnabled = true;
             arrowUp.input.enableDrag(true);
             arrowUp.input.allowVerticalDrag = false;
-           
         }
         else if(rndnr==4){
             arrowDown = this.game.add.sprite(this.game.world.randomX,0,'ghost');
-            this.game.physics.enable( [arrowDown ], Phaser.Physics.ARCADE);
+            this.game.physics.enable( [ arrowDown ], Phaser.Physics.ARCADE);
             arrowDown.body.collideWorldBounds = true;
             arrowDown.inputEnabled = true;
             arrowDown.input.enableDrag(true);
             arrowDown.input.allowVerticalDrag = false;
-        
-       
-    }
-},
+        }
+    },
     
     quitGame:function() {
-        
         this.state.start('StartMenu');
     },
     
     
     update: function() {
         
-        }
+    }
     
     
 };
