59e999f401b2c9bd130343d3ecdf829ec807dbe4
Erikmannen <erik.azp@hotmail.se>
2016-11-07 13:00:33 +0100

    mute

--- swiper/Settings.js
+++ swiper/Settings.js
@@ -7,11 +7,24 @@ Settings.prototype = {
     	//Are there no image?
     	//Add the "Back" button
         back = this.game.add.button(this.game.world.centerX - 130, 800, 'back', this.backMenu, this, 2, 1, 0);
+        mute = this.game.add.button(this.game.world.centerX - 130, 400, 'mute', this.Mute, this, 2, 1, 0);
 
     },
     //If "Back" button is pressed
     backMenu: function(){
         //Go to start menu state
         this.game.state.start('StartMenu');
+    },
+
+    Mute: function(){
+    	this.game.sound.mute = true;
+    	mute.pendingDestroy = true;
+    	unMute = this.game.add.button(this.game.world.centerX - 130, 400, 'unMute', this.unMute, this, 2, 1, 0);
+    },
+
+    unMute: function(){
+    	this.game.sound.mute = false;
+    	mute = this.game.add.button(this.game.world.centerX - 130, 400, 'mute', this.Mute, this, 2, 1, 0);
+    	unMute.pendingDestroy = true;
     }
 };
--- swiper/StartMenu.js
+++ swiper/StartMenu.js
@@ -8,16 +8,11 @@ StartMenu.prototype = {
 
 	var StartMenu = this.game.add.image(0,0,'titlescreen');
 
-
+	//Add the buttons
 	button = this.game.add.button(this.game.world.centerX - 155, 430, 'button', this.startGame, this, 2, 1, 0);
 	HS_button = this.game.add.button(this.game.world.centerX - 130, 570, 'HS_button', this.startHighscoreMenu, this, 2, 1, 0);
 	S_button = this.game.add.button(this.game.world.centerX - 130, 680, 'S_button', this.startSettings, this, 2, 1, 0);
 	//Q_button = this.game.add.button(this.game.world.centerX = 130, 790, 'Q_button', this.Quit, this, 2, 1, 0);
-		//Add the buttons
-		button = this.game.add.button(this.game.world.centerX - 155, 430, 'button', this.startGame, this, 2, 1, 0);
-		HS_button = this.game.add.button(this.game.world.centerX - 130, 570, 'HS_button', this.startHighscoreMenu, this, 2, 1, 0);
-		S_button = this.game.add.button(this.game.world.centerX - 130, 680, 'S_button', this.startSettings, this, 2, 1, 0);
-
 	},
 
 	//If button "Start" is pressed this function will be executed
