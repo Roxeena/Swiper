a37971d2b65b750a897245b4ca4af40ef4007877
Malin <malin.ejdbo@gmail.com>
2016-11-08 15:39:47 +0100

    Svårighetsgrad
    Tagit bort gravity satt en konstant hastighet ist. Hastigheten ökar
    beroende på vilken level man är på

--- swiper/Game.js
+++ swiper/Game.js
@@ -2,12 +2,13 @@ var Game = function(game) {
     //Declare some variables 
     this.gameover;
     this.countdown;
-    this.secondsElapsed;
     this.timer;
     this.generateImage;
 };
 
-//Declare more variables??
+//Declare more variables
+var timer;
+var secondsElapsed = 0;
 var counterlives = 5;
 var scoreText;
 var score = 0;
@@ -18,18 +19,19 @@ var Width=540;
 var Height=960;
 var Level=0;
 var rndnr;
+var velocityStart = 150;
+var numSecPerLev= 10;
 
 Game.prototype = {
     create: function() {   
         //Initialize some settings and "meta data"
-        this.gameover = false;
-        this.secondsElapsed = 0;                        //Is this requierd when we have a timer?
-        this.timer = this.time.create(false);           //This timer is never used, it is started but never used
-        this.timer.loop(1000, this.updateSeconds, this);
+        this.gameover = false;                        
+        timer = this.time.create(false);          
+        timer.loop(1000, this.updateSeconds, this);
+        timer.start();
 
         //Start the physics of the game, gravity
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.physics.arcade.gravity.y = 100;
 
         //Build world
         this.buildWorld();
@@ -37,7 +39,11 @@ Game.prototype = {
     
     updateSeconds: function() {
         //Update the variable with how many seconds have elapsed
-        this.secondsElapsed++;       
+        secondsElapsed++;
+        //Update the level with time
+        if((secondsElapsed % numSecPerLev) == 0){
+            ++Level;
+        }       
     },
 
     buildWorld: function() {    //Build the game
@@ -55,13 +61,13 @@ Game.prototype = {
         music = this.game.add.audio('jerry');
         music.play();
 
-        //Start the timer
-        this.timer.start();
+        //Start the timer 
+        //this.timer.start();
 
         //Add objects in loop depending on time
         //Spawn an object every 2 seconds        
 
-        this.game.time.events.repeat(Phaser.Timer.SECOND * (2/(Level+1)), 34, this.generateImage, this);
+        this.game.time.events.repeat(Phaser.Timer.SECOND * (2), 34, this.generateImage, this);
        
 
         //Quit after a certain amount of time, music ends
@@ -70,7 +76,6 @@ Game.prototype = {
 
     generateImage: function() {     //Spawn an object
         //Create a random number between 1 and 2
-
         rndnr = this.game.rnd.integerInRange(1, 2);
 
         //Random X-position for spwan
@@ -78,23 +83,26 @@ Game.prototype = {
         spwnrng = spwnrng +this.game.rnd.integerInRange(200, 400);
         
         //Create a random object to spawn
-        //If the random number is 1, then spawn a bunny, right arrow
+        //If the random number is 1, then spawn a right arrow
         if (rndnr == 1){
-            //Add a bunny, right arrow
+            //Add a right arrow
             arrowRight = this.game.add.sprite(spwnrng,0,'proto_right_pil'); 
             selected = arrowRight;
 
         }
-        //If the random number is 2, then spawn a spacefighter, left arrow     
+        //If the random number is 2, then spawn a left arrow     
         if (rndnr==2){
             //Add a spacefighter, left arrow
             arrowLeft = this.game.add.sprite(spwnrng,0,'proto_left_pil');
             selected = arrowLeft;
         }
      
-        //Enable gravity for new spawned object
+        //Enable physics for new spawned object
         this.game.physics.enable( [ selected ], Phaser.Physics.ARCADE);
-         
+        
+        //Set the velocity for the object
+        selected.body.velocity.y = velocityStart + (Level + 1) * 10;
+        
         //Enalbe swiping
         selected.inputEnabled = true;
         selected.input.enableDrag(true);
@@ -120,15 +128,9 @@ Game.prototype = {
                 //Update the score. Why? Isnt this function only for when losing lives? 
                 ++score;
                 scoreText.setText( 'Score: '+score );
-                //så det går snabbare.
-                if(score % (Level*5)==0)
-                {
-                   ++Level;
-                }
-                 this.game.physics.arcade.gravity.y = this.game.physics.arcade.gravity.y +(Level*25);
                 
             }
-                else
+            else
             {
             //Decrement the number of lives
             --counterlives;
@@ -186,7 +188,6 @@ Game.prototype = {
     
     
     update: function() {
-       
     }
     
     
